4788f805b567cd1920e97b97b51e91c5d75941fc
bobjoe400 <cnamattern@gmail.com>
2016-05-09 21:11:48 -0700

    Completed problem 4

--- CPE102/src/lab6/Reverse.java
+++ CPE102/src/lab6/Reverse.java
@@ -1,19 +1,10 @@
 package lab6;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Reverse {
-
-	
 	public static void main(String[] args){
-		try {
-			Scanner in = new Scanner(new File(args[0]));
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Reverser rev = new Reverser(new File(args[0]));
+		rev.getReverse();
 	}
 }
--- CPE102/src/lab6/Reverser.java
+++ CPE102/src/lab6/Reverser.java
@@ -1,5 +1,46 @@
 package lab6;
 
-public class Reverser {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Reverser {
+	
+	private ArrayList<String> data;
+	
+	public Reverser(File file){
+		data = new ArrayList<String>();
+		Scanner in = null;
+		try {
+			in = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(in.hasNextLine()){
+			data.add(in.nextLine());
+		}
+		in.close();
+		file.delete();
+	}
+	
+	public void getReverse(){
+		File f = new File("input.txt");
+		try{
+			PrintWriter pr = new PrintWriter(f);
+			for(String s: data){
+				String rev = "";
+				for(int i = s.length()-1; i >= 0; i--){
+					rev+=s.charAt(i);
+				}
+				pr.write(rev);
+				pr.write("\n");
+			}
+			pr.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
